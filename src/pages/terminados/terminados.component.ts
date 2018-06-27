e407a23740f4a71c8ef769f01bdcc4fc984dfb8d
claudio <candradeg9182@gmail.com>
2018-06-26 23:12:50 -0600

    feat: clase1. Creando los unicos components 'pendientes'  y 'terminados'

---
+++ src/pages/terminados/terminados.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component ({
+    selector: 'app-terminados',
+    templateUrl: 'terminados.component.html',
+})
+
+export class terminadosComponent implements OnInit {
+
+    constructor(){}
+
+    ngOnInit(){}
+
+}
